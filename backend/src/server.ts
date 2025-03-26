98a442cbed2a30f60d14b951078a7005e2a34f94
Paul <pmi22@cornell.edu>
2025-03-26 10:58:19 -0400

    need to deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,13 @@ import app from './app'; // Ensure the relative path is correct
 console.log('App imported successfully');
 import { connectDB } from './db';
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
 (async () => {
     const db = await connectDB();
     console.log('Connected to MongoDB:', db.databaseName);
 })();
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
